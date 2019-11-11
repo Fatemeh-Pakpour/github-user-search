524e91fce1ab64e4057f2145841fc22d65fa82d8
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-11 13:35:40 +0100

    added the Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Consumer } from "./Context";
+
+const Loading = props => {
+  return (
+    <Consumer>
+      {({ loading }) => {
+        if (loading) {
+          return <h1>Loading ...</h1>;
+        } else {
+          return props.children;
+        }
+      }}
+    </Consumer>
+  );
+};
+
+export default Loading;
