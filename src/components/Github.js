0a516e65dc207c545c745f7b39d07becddbbab16
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-11 13:25:47 +0100

    added the Github.js file

---
+++ src/components/Github.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from "react-bootstrap";
+const Github = props => {
+  const path = `/users/${props.userAccount}`
+  return (
+    <Card className="user-card">
+      <Card.Img variant="top" className="image" src={props.url} alt="" />
+      <Link to ={path}>
+        <Card.Text>{props.userAccount}</Card.Text>
+      </Link>
+    </Card>
+  );
+};
+export default Github
